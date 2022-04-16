681c8714da6b20cdc1ae408ad4fa5bcfc23d141d
Asim Mirza <amirza@axway.com>
2022-04-17 06:36:07 +1000

    minor updates

--- build_anm_image.sh
+++ build_anm_image.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 tar xf APIGateway_7.7.20220228-DockerScripts-2.4.0.tar
-$APIGATEWAY_LICENSE | base64 -d >license.lic
+echo $APIGATEWAY_LICENSE | base64 -d >license.lic
 ./apigw-emt-scripts-2.4.0/build_anm_image.py --license=license.lic --domain-cert=certs/mydomain/mydomain-cert.pem --domain-key=certs/mydomain/mydomain-key.pem --domain-key-pass-file=certs/mydomain/mydomain.pass.txt --merge-dir anm/merge-dir/apigateway --metrics --anm-username=admin --anm-pass-file=anm/anm.pass.txt --parent-image=romaicus/apim_base:77-20220416 --out-image=romaicus/apim_anm:77-20220416
