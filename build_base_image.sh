2ed93ffaf33bcf85533f6abbe69972cb5e5e1db7
Asim Mirza <amirza@axway.com>
2022-04-17 07:12:36 +1000

    using exported license environment variable

--- build_base_image.sh
+++ build_base_image.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 tar xf APIGateway_7.7.20220228-DockerScripts-2.4.0.tar
 ./apigw-emt-scripts-2.4.0/build_base_image.py --installer=$HOME/APIGateway_7.7.20220228_Install_linux-x86-64_BN02.run --os=centos7 --out-image=romaicus/apim_base:77-20220416
+docker tag romaicus/apim_base:77-20220416 apim_base:latest
+docker push romaicus/apim_base:77-20220416
+docker push romaicus/apim_base:latest
